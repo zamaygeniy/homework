a581af890dae2c1a8da8858667dbc75c69590edb
mariwaldo <55176463+mariwaldo@users.noreply.github.com>
2019-09-24 23:52:06 +0300

    Add files via upload

---
+++ 2019.09.11/Task1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Enter a, b, c " << endl;
+	int a, b, c;
+	cin >> a >> b >> c;
+	int min = a;
+
+	if (b < min)
+		min = b;
+	if (c < min)
+		min = c;
+
+	cout << "min = " << min << endl;
+
+}
