c133df4208513997c0cebb618658016f0141a254
mariwaldo <55176463+mariwaldo@users.noreply.github.com>
2019-09-25 00:29:08 +0300

    Add files via upload

---
+++ 2019.09.11/Task3_2.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Enter coordinates x, y" << endl;
+	double x, y;
+	cin >> x >> y;
+
+	if (x >= 0)
+		if (x*x + y * y <= 1)
+			cout << "point is inside" << endl;
+		else
+			cout << "point isn't inside" << endl;
+	else
+		if (y <= x + 1 && y >= -1 - x)
+			cout << "point is inside" << endl;
+		else
+			cout << "point isn't inside" << endl;
+
+	return 0;
+}
