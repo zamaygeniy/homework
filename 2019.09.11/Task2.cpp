b31b14f769c1808e6c50bf82255f020810bef13d
mariwaldo <55176463+mariwaldo@users.noreply.github.com>
2019-09-25 00:05:20 +0300

    Add files via upload

---
+++ 2019.09.11/Task2.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	cout << "Enter a, b, c" << endl;
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a + b > c && b + c > a && a + c > b)
+		if (a == b && b == c)
+			cout << "equilateral triangle" << endl;
+		else
+			if (a == b || a == c || b == c)
+				cout << "isosceles triangle" << endl;
+			else
+				cout << "versatile triangle" << endl;
+	else
+		cout << "such triangle doesn't exist" << endl;
+
+	return 0;
+}
