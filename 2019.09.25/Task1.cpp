773fbffb1983aa33e36884f266551478952dc5d5
VladislavDubyaga <55176463+VladislavDubyaga@users.noreply.github.com>
2019-09-29 02:50:29 +0300

    Add files via upload

---
+++ 2019.09.25/Task1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	cout << "enter n, x" << endl;
+	double x;
+	int n;
+	cin >> n >> x;
+
+	cout << "e^x = " << setprecision(15) << exp(x) << endl;
+
+	double k = x, sum = 1, l = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		l *= i;
+		sum += k / l;
+		k *= x;
+	}
+	cout << "sum = " << sum << endl;
+
+
+}
+
+
