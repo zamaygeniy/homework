3c2082fd07398948d73865a47be4c57cc9e5eac5
Vladislav Dubyaga <55176463+zamaygeniy@users.noreply.github.com>
2019-10-08 23:47:31 +0300

    Add files via upload

---
+++ 2019.10.02/task1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	double i;
+	cout << "enter the number from 1 to 10" << endl;
+	while (true)
+	{
+		cin >> i;
+		if (i > 0 && i < 11 && i == int(i))
+			break;
+		else
+			cout << "error. Input correct number" << endl;
+	}
+
+	i = ++i / 2;
+	switch (int(i))
+	{
+		case 1: 
+		{
+			cout << "was absent" << endl;
+			break;
+		}
+		case 2:
+		{
+			cout << "not satisfactorily" << endl;
+			break;
+		}
+		case 3:
+		{
+			cout << "satisfactorily" << endl;
+			break;
+		}
+		case 4:
+		{
+			cout << "good" << endl;
+			break;
+		}
+		case 5:
+		{
+			cout << "excellent" << endl;
+		}
+
+	}
+	
+}
+
---
+++ 2019.10.02/task2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	double i;
+	cout << "enter the number from 1 to 12" << endl;
+	while (true)
+	{
+		cin >> i;
+		if (i > 0 && i < 13 && i == int(i))
+			break;
+		else
+			cout << "error. Input correct number" << endl;
+	}
+
+	i = float(i / 3) + 1;
+	switch (int(i))
+	{
+		case 2:
+		{
+			cout << "spring" << endl;
+			break;
+		}
+		case 3: 
+		{
+			cout << "summer" << endl;
+			break;
+		}
+		case 4:
+		{
+			cout << "fall" << endl;
+			break;
+		}
+		default: cout << "winter" << endl;
+	}	
+}
+
