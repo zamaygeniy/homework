f922ec3d8eab106b8bdf6f12f7a6b954d1b40bf2
Vladislav Dubyaga <55176463+zamaygeniy@users.noreply.github.com>
2019-11-13 12:29:16 +0300

    Add files via upload

---
+++ 2019.10.30/Task1234.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+void input_array(int* arrayref) {
+	for (int i = 0; i < 10; i++) {
+		cin >> arrayref[i];
+	}
+}
+
+void task1(int* arrayref)
+{
+	int j = 9;
+	for (int i = 0; i < 5; i++)
+	{
+		arrayref[i] += arrayref[j];
+		arrayref[j] = arrayref[i] - arrayref[j];
+		arrayref[i] -= arrayref[j];
+		j--;
+	}
+}
+
+void task2(int* arrayref)
+{
+	int k = 0;
+	for (int i = 0; i < 10; i++)
+	{	
+		if (arrayref[i] < 0) arrayref[i] = 0;
+		for (int j = 2; j <= sqrt(arrayref[i]); j++)
+			if (arrayref[i] % j == 0)
+				k++;
+		if (k > 0)
+			arrayref[i] = 0;
+		k = 0;
+	}
+
+	for (int i = 0; i < 10; i++)
+		if (arrayref[i] == 0) {}
+		else
+			cout << arrayref[i] << "  ";
+}
+
+void task3(int* arrayref)
+{
+	cout << "enter n = ";
+	int n;
+	cin >> n;
+	int i = 9;
+	while (true)
+	{
+		if (arrayref[i] >= n)
+			i--;
+		else
+			break;
+	}
+
+	cout << "number of elements = " << i + 1;
+
+
+}
+
+void task4(int* arrayref)
+{
+	int equal = 0, more = 0, less = 0;
+	for (int i = 1; i < 10; i++)
+		if (arrayref[i] == arrayref[i - 1] && equal == 0)
+			equal++;
+		else
+			if (arrayref[i] < arrayref[i - 1] && less == 0)
+				less++;
+			else
+				if (arrayref[i] > arrayref[i - 1] && more == 0)
+					more++;
+
+
+	
+			
+}
+
+int main()
+{
+	int array[10];
+	int* arrayref = &array[0];
+	input_array(arrayref);
+	task3(arrayref);
+
+
+
+
+}
