2440600fe479d1e99cfa07cda707e6eacc27cc37
Vladislav Dubyaga <55176463+zamaygeniy@users.noreply.github.com>
2019-11-21 08:41:06 +0300

    Update sort.cpp

--- 2019.11.06/sort.cpp
+++ 2019.11.06/sort.cpp
@@ -2,50 +2,60 @@
 
 using namespace std;
 
-void swap(int&, int&);
-void sort(int*, int);
-int* allocateMemory(int);
-void deleteMemory(int*);
-void input_array(int*, int);
-void output_array(int*, int);
+void Swap(int&, int&);
+void Sort(int*, int);
+int* AllocateMemory(int);
+void DeleteMemory(int*);
+void InputArray(int*, int);
+void OutputArray(int*, int);
 
-void input_array(int* arrayref, int n)
+void InputArray(int* arrayref, int n)
 {
-	for (int i = 0; i < n; i++) cin >> arrayref[i];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arrayref[i];
+	}
 }
 
-void output_array(int* arrayref, int n)
+void OutputArray(int* arrayref, int n)
 {
 	cout << endl;
-	for (int i = 0; i < n; i++) cout << arrayref[i] << " ";
+	for (int i = 0; i < n; i++)
+	{
+		cout << arrayref[i] << " ";
+	}
 }
 
-void  deleteMemory(int* sourceArray)
+void  DeleteMemory(int* sourceArray)
 {
 	delete[]sourceArray;
 }
 
-int* allocateMemory(int n)
+int* AllocateMemory(int n)
 {
 	return new int[n];
 }
 
-void swap(int& a, int& b)
+void Swap(int& a, int& b)
 {
 	a = a + b;
 	b = a - b;
 	a = a - b;
 }
 
-void sort(int* sourceArray, int n)
+void Sort(int* sourceArray, int n)
 {
 	for (int i = 0; i < n - 1; i++)
 	{
 		int min = i;
 		for (int j = i + 1; j < n; j++)
-			if (sourceArray[j] < sourceArray[min]) min = j;
+		{
+			if (sourceArray[j] < sourceArray[min]) 
+				min = j;
+		}
+		
 		if (min != i)
-			swap(sourceArray[i], sourceArray[min]);
+			Swap(sourceArray[i], sourceArray[min]);
 	}
 }
 
@@ -55,11 +65,8 @@ int main()
 	cin >> n;
 	int* array = allocateMemory(n);
 	int* arrayref = &array[0];
-
-
-	input_array(arrayref, n);
-	sort(array, n);
-	output_array(arrayref, n);
-
+	
+	InputArray(arrayref, n);
+	Sort(array, n);
+	OutputArray(arrayref, n);
 }
-
