1a571feb326d3ecedc3df0b2e8d44e7537be985e
Vladislav Dubyaga <55176463+zamaygeniy@users.noreply.github.com>
2019-12-04 02:00:46 +0300

    Add files via upload

---
+++ 2019.11.27/unknownSize.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int size = 1;
+	char* str = new char[size];
+
+	char value;
+	int i = 0;
+
+	while (cin.get(value))
+	{
+		if (value == '\n')
+		{
+			break;
+		}
+
+		i++;
+		if (i >= size)
+		{
+			int newsize = size * 2;
+			char* newstr = new char[newsize];
+
+			for (int j = 0; j < size; j++)
+			{
+				newstr[j] = str[j];
+			}
+			delete[] str;
+			size = newsize;
+			str = newstr;
+		}
+		str[i] = value;
+	}
+
+	for (int j = 1; j <= i; j++)
+		cout << str[j];
+
+	return 0;
+}
+
