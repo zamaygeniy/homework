f68cd757eb1b605bcaaed19fa65c1cade2d947ed
Vladislav Dubyaga <55176463+zamaygeniy@users.noreply.github.com>
2020-05-22 01:14:30 +0300

    ПЕРЕМЕСТИЛ ФУНКЦИИ ПОД МЕЙН
    (ПРОТОТИПЫ БЫЛИ)

--- 2020.04.12/IndividualTask.cpp
+++ 2020.04.12/IndividualTask.cpp
@@ -1,86 +1,93 @@
 #include <iostream>
 #include <fstream>
-
+#include <cmath>
 using namespace std;
 
-int Side();
-void Input(int**, int);
-void Output(int**, int);
-void Transformation(int**, int**, int);
-int** AllocateMemory(int);
-int* _AllocateMemory(int);
+void input(int**, int, const char*);
+void output(int**, int, const char*);
+void transformation(int**, int**, int);
+int** allocateMatrix(int);
+int* allocateArray(int);
+int matrixOrder(const char*);
 
-int Side() {
+int main()
+{
+    const char* inPath = "in.txt";
+    const char* outPath = "out.txt";
+    int side = matrixOrder(inPath);
+
+    int** firstMatrix = allocateMatrix(side);
+    for (int i = 0; i < side; i++) {
+        firstMatrix[i] = allocateArray(side);
+    }
+
+    int** secondMatrix = allocateMatrix(side);
+    for (int i = 0; i < side; i++) {
+        secondMatrix[i] = allocateArray(side);
+    }
+
+    input(firstMatrix, side, inPath);
+    transformation(firstMatrix, secondMatrix, side);
+    output(secondMatrix, side, outPath);
+}
+
+
+int matrixOrder(const char* path) {
     ifstream in;
-    in.open("D:\\in.txt");
-    if (!in.is_open())
-    {
-        cout << "";
+    in.open(path);
+    if (!in.is_open()) {
+        cout << "Input file is not open\n";
     }
     int temp;
     int count = 0;
-    while (in >> temp)
-    {
+    while (in >> temp) {
         count++;
     }
-
     in.clear();
-    in.seekg(0, ios::beg);
+    in.seekg(0);
     in.close();
     return sqrt(count);
 }
 
-void Input(int** A, int side) {
+void input(int** matrix, int side, const char* path) {
     ifstream in;
-    in.open("D:\\in.txt");
-
-    for (int i = 0; i < side; i++)
-    {
-        for (int j = 0; j < side; j++)
-        {
-            in >> A[i][j];
+    in.open(path);
+    for (int i = 0; i < side; i++) {
+        for (int j = 0; j < side; j++) {
+            in >> matrix[i][j];
         }
     }
-
     in.close();
 }
 
-void Output(int** B, int side) {
-    std::ofstream out("D:\\in.txt", std::ios::app);
+void output(int** matrix, int side, const char* path) {
+    std::ofstream out(path, std::ios::app);
     out << endl;
-    for (int i = 0; i < side; i++)
-    {
+    for (int i = 0; i < side; i++) {
         out << endl;
-        for (int j = 0; j < side; j++)
-        {
-            out << B[i][j] << " ";
+        for (int j = 0; j < side; j++) {
+            out << matrix[i][j] << " ";
         }
     }
     out.close();
 }
 
-void Transformation(int** A, int** B, int side) {
-    for (int i = 0; i < side; i++)
-    {
-        for (int j = i; j < side; j++)
-        {
-            if (i == j)
-            {
-                B[i][j] = A[i][j];
+void transformation(int** first, int** second, int side) {
+    for (int i = 0; i < side; i++) {
+        for (int j = i; j < side; j++) {
+            if (i == j) {
+                second[i][j] = first[i][j];
                 continue;
             }
-            B[i][j] = A[0][0];
-            B[j][i] = A[0][0];
-            for (int k = 0; k < side; k++)
-            {
-                for (int l = k; l < side; l++)
-                {
-                    if (k <= i || l >= j)
-                    {
-                        if (A[k][l] >= B[i][j])
-                            B[i][j] = A[k][l];
-                        if (A[l][k] >= B[j][i])
-                            B[j][i] = A[l][k];
+            second[i][j] = first[0][0];
+            second[j][i] = first[0][0];
+            for (int k = 0; k < side; k++) {
+                for (int l = k; l < side; l++) {
+                    if (k <= i || l >= j) {
+                        if (first[k][l] >= second[i][j])
+                            second[i][j] = first[k][l];
+                        if (first[l][k] >= second[j][i])
+                            second[j][i] = first[l][k];
                     }
                 }
             }
@@ -88,31 +95,10 @@ void Transformation(int** A, int** B, int side) {
     }
 }
 
-int** AllocateMemory(int side) {
+int** allocateMatrix(int side) {
     return new int* [side];
 }
 
-int* _AllocateMemory(int side) {
+int* allocateArray(int side) {
     return new int[side];
-}
-
-int main()
-{
-    int side = Side();
-
-    int** A = AllocateMemory(side);
-    for (int i = 0; i < side; i++)
-    {
-        A[i] = _AllocateMemory(side);
-    }
-
-    int** B = AllocateMemory(side);
-    for (int i = 0; i < side; i++)
-    {
-        B[i] = _AllocateMemory(side);
-    }
-
-    Input(A, side);
-    Transformation(A, B, side);
-    Output(B, side);
 }
